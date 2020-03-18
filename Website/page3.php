b669fcf7fc625137f0210ec8741aace64b1eaf6e
Sam-A0 <53252880+Sam-A0@users.noreply.github.com>
2020-03-18 12:06:49 +1100

    stage 2 website
    this is just template evrything else that is unessessary is removed

---
+++ Website/page3.php
@@ -0,0 +1,19 @@
+<?php
+	include("header.inc");
+?>
+
+<?php
+	include("nav.inc");
+?>
+
+
+<main id=cous>
+
+</main>
+
+
+
+
+<?php
+	include("footer.inc");
+?>
---
+++ Website/page4.php
@@ -0,0 +1,25 @@
+<?php
+	include("header.inc");
+?>
+
+
+<?php
+	include("nav.inc");
+?>
+
+
+	<main id=cous>
+		
+		
+		
+	
+	</main>
+
+
+
+
+
+
+<?php
+	include("footer.inc");
+?>
