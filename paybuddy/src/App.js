6555d7d1dd44ceae85dbe38c2cb2bda8147d42d6
s3539822 <s3539822@student.rmit.edu.au>
2020-03-27 22:38:09 +1100

    Update
    Product Selection and Checkout pages completed
    No update in user record in db

--- paybuddy/src/App.js
+++ paybuddy/src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import React, { useState, useRef, useEffect } from 'react';
-import chair from './chair.jpg';
 import gif from './giphy.gif';
 import Logo from './Icon.png';
+
 
 function Product({ product }) {
   const [paidFor, setPaidFor] = useState(false);
@@ -17,13 +17,15 @@ function Product({ product }) {
           label: 'checkout',
           color: 'white',
           shape: 'pill',
+          tagline: 'false',
+          height: 48
         },
         createOrder: (data, actions) => {
           return actions.order.create({
-            purchase_units: [{
+            purchase_units: [
+              {
                 description: product.description,
                 amount: {
-                  currency_code: 'USD',
                   value: product.price,
                 },
               },
@@ -44,41 +46,38 @@ function Product({ product }) {
   }, [product.description, product.price]);
 
   if (paidFor) {
-    return (
-      <div>
-        <h1>Congrats, you just bought {product.name}!</h1>
-        <img alt={product.description} src={gif} />
-      </div>
-    );
+    {/* PAYPAL DEPOSIT SUCCEFUL - REROUTE TO CONFIRMATION PAGE */}
   }
 
   return (
     <div>
       {error && <div>Uh oh, an error occurred! {error.message}</div>}
-      <div class="_00001" id="block-id">
-        <div class="_00002" id="header-id">
-          <div class="_00003">
-            <h2 class="_00004">Choose deposit amount</h2>
-          </div>
+      <div ref={paypalRef} />
+    </div>
+  );
+}
+
+const ProductSelection = props => {
+  return (
+    <div>
+      <div class="_3o1Fr" id="block-id">
+        <div class="_3mYpM" id="header-id">
+          <h2 class="_00004">Select deposit amount</h2>
         </div>
-        <ul class="_00005" id="body-id">
+        <ul class="_23RYk" id="body-id">
           <li class="_2j5BM">
             <div class="_1fm9z">
               <div class="_25xBd _2jWsC _2LHSO">
-                <div class="_1GLWk"></div>
-                <div class="rMY2T _3kqWw _2-iar">
+                <div class="_1GLWk" id="box-header-id"></div>
+                <div class="rMY2T _3kqWw _2-iar" id="box-body-id">
                   <div class="_2vRlH">
-                    <div class="_3RQLA">
-                      <span class="_16YfQ">
-                        <span>
-                          <img class="logo-image" src={Logo}/>
-                        </span>
-                      </span>
-                      <h3 class="GMZpj">
-                        Australia
-                        <small>PayBuddy Credit</small>
-                      </h3>
-                    </div>
+                    <span class="_16YfQ">
+                      <img class="logo-image" src={Logo}/>
+                    </span>
+                    <h3 class="GMZpj">
+                      Australia
+                      <small>PayBuddy Deposit</small>
+                    </h3>
                   </div>
                   <div class="WekeV">
                     <div class="_3PiID _2wSlB">
@@ -87,8 +86,8 @@ function Product({ product }) {
                         <div class="_1euEK"></div>
                       </div>
                       <div class="em8W4">
-                        <div class="_3GfbC _3dX_-">
-                          <button class="IzjkL _2Y_WL FiOTW">
+                        <div class="_3dX_-">
+                          <button class="IzjkL _2Y_WL FiOTW" id="selection-1" onClick={props.addToCart1}>
                             <span class="_2pjgR">Continue</span>
                           </button>
                         </div>
@@ -105,17 +104,13 @@ function Product({ product }) {
                 <div class="_1GLWk"></div>
                 <div class="rMY2T _3kqWw _2-iar">
                   <div class="_2vRlH">
-                    <div class="_3RQLA">
-                      <span class="_16YfQ">
-                        <span>
-                          <img class="logo-image" src={Logo}/>
-                        </span>
-                      </span>
-                      <h3 class="GMZpj">
-                        Australia
-                        <small>PayBuddy Credit</small>
-                      </h3>
-                    </div>
+                    <span class="_16YfQ">
+                      <img class="logo-image" src={Logo}/>
+                    </span>
+                    <h3 class="GMZpj">
+                      Australia
+                      <small>PayBuddy Deposit</small>
+                    </h3>
                   </div>
                   <div class="WekeV">
                     <div class="_3PiID _2wSlB">
@@ -124,8 +119,8 @@ function Product({ product }) {
                         <div class="_1euEK"></div>
                       </div>
                       <div class="em8W4">
-                        <div class="_3GfbC _3dX_-">
-                          <button class="IzjkL _2Y_WL FiOTW">
+                        <div class="_3dX_-">
+                          <button class="IzjkL _2Y_WL FiOTW" id="selection-2" onClick={props.addToCart2}>
                             <span class="_2pjgR">Continue</span>
                           </button>
                         </div>
@@ -142,27 +137,23 @@ function Product({ product }) {
                 <div class="_1GLWk"></div>
                 <div class="rMY2T _3kqWw _2-iar">
                   <div class="_2vRlH">
-                    <div class="_3RQLA">
-                      <span class="_16YfQ">
-                        <span>
-                          <img class="logo-image" src={Logo}/>
-                        </span>
-                      </span>
-                      <h3 class="GMZpj">
-                        Australia
-                        <small>PayBuddy Credit</small>
-                      </h3>
-                    </div>
+                    <span class="_16YfQ">
+                      <img class="logo-image" src={Logo}/>
+                    </span>
+                    <h3 class="GMZpj">
+                      Australia
+                      <small>PayBuddy Deposit</small>
+                    </h3>
                   </div>
                   <div class="WekeV">
                     <div class="_3PiID _2wSlB">
                       <div class="_76Q-5">
-                        <div class="_3CENX">AU$15.00</div>
+                        <div class="_3CENX">AU$25.00</div>
                         <div class="_1euEK"></div>
                       </div>
                       <div class="em8W4">
-                        <div class="_3GfbC _3dX_-">
-                          <button class="IzjkL _2Y_WL FiOTW">
+                        <div class="_3dX_-">
+                          <button class="IzjkL _2Y_WL FiOTW" id="selection-3" onClick={props.addToCart3}>
                             <span class="_2pjgR">Continue</span>
                           </button>
                         </div>
@@ -175,27 +166,125 @@ function Product({ product }) {
           </li>
         </ul>
       </div>
-
-
-      
-      {/* <div ref={paypalRef} /> */}
     </div>
-  );
+  )
 }
 
-function App() {
+const PayPalCheckout = props => {
+
   const product = {
-    price: 787.77,
-    name: 'comfy chair',
-    description: 'fancy chair, like new',
-    image: chair,
+    price: props.cartValue,
+    name: 'PayBuddy deposit',
+    description: 'PayBuddy funds deposit'
   };
 
   return (
     <div className="App">
-      <Product product={product} />
+      <div class="_3o1Fr" id="block-id">
+        <div class="_3mYpM" id="header-id">
+          <h2 class="_00004">Confirm order and pay</h2>
+        </div>
+        <div class="_2xuF0" aria-live="assertive">
+          <div class="_3YW0V">
+            <div class="_2OPBO">
+              <div class="_32Oc5 _2onyo">
+                <div class="_1zJje">
+                  <span>
+                    <span>
+                      By clicking "Pay now", you agree to the <a class="_2rLR4" role="link" target="_blank">PayPuddy Services Agreement</a> and <a class="_2rLR4" role="link" target="_blank">Privacy and Cookies policy</a>, and you authorize PayBuddy to store your payment details.
+                    </span>
+                  </span>
+                </div>
+                <div class="_1zJje">
+                  <span></span>
+                </div>
+                <div class="_1zJje">
+                  <span></span>
+                </div>
+              </div>
+              <div class="_3msgu">
+                <div class="_lksW2">
+                  <Product product={product} />
+                </div>
+                <div class="_lksW2">
+                  <button id="cancelButton" onClick={props.cancelCart} class="_16apt _2Y_Wp _2sGmV">
+                    <span>Cancel</span>
+                  </button>
+                </div>                
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="c1vnA">
+          <div class="jvzF1">
+            <div class="_1cMq5">
+              <div class="_1l2tM _nfdh5">Your order</div>
+            </div>
+            <div class="_1cMq5">
+              <div class="_nfdh4">PayBuddy Deposit</div>
+            </div>
+            <div class="_1cMq5 _1Fz0I">
+              <div class="_6jTY7">
+                <span class="_35e-z">Total</span>
+              </div>
+              <div class="_Drf47" data-basket-field="totalPrice"><b>AU${props.cartValue}.00</b></div>
+            </div>
+            <div class="TgisE">
+              <span>
+                In some cases, your default currency will not be supported by a payment method directly. To solve this we convert the cost of your order automatically at prevailing European Central Bank rates.
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+class FundsDeposit extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { showCheckout: false, cartValue: 10}
+    this.addToCart = this.addToCart.bind(this);
+    this.cancelCart = this.cancelCart.bind(this);
+  }
+
+  addToCart1 = () => {
+    this.addToCart(5)
+  }
+
+  addToCart2 = () => {
+    this.addToCart(10)
+  }
+
+  addToCart3 = () => {
+    this.addToCart(25)
+  }
+
+  addToCart = (e) => {
+    this.setState({
+      ...this.state,
+      showCheckout: true,
+      cartValue: e
+    })
+  }
+
+  cancelCart = () => {
+    this.setState({
+      ...this.state,
+      showCheckout: false
+    })
+  }
+
+  render() {
+    return (
+      <div>
+       {!this.state.showCheckout ? <ProductSelection addToCart1={this.addToCart1} addToCart2={this.addToCart2} addToCart3={this.addToCart3} />
+                                 : <PayPalCheckout cartValue={this.state.cartValue} cancelCart={this.cancelCart} />}
+      </div>
+    );
+  }
+}
+
+export default FundsDeposit;
