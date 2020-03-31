612d48044453300ff7e599f046de034802d19a0b
s3539822 <s3539822@student.rmit.edu.au>
2020-03-31 21:13:29 +1100

    Update

--- paybuddy_v2/client/src/App.js
+++ paybuddy_v2/client/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import FundsDeposit from './FundsDeposit';
 
@@ -24,26 +23,6 @@ class App extends React.Component {
     return (
       <div className="App">
         <FundsDeposit />
-        
-        {/* <h4> class="header center") Recent Votes</h4>
-        <ul class="container collection center">
-          {for(vote : apiResponse)
-          li(class="collection-item avatar")
-            if vote.candidate.trim() === 'TABS'
-              i(class="material-icons circle green") keyboard_tab
-            else
-              i(class="material-icons circle blue") space_bar
-            span(class="title") A vote for <b>#{vote.candidate}</b>
-            p was cast at #{vote.time_cast}.
-            </ul> */}
-        
-        
-        
-        
-        
-        
-        
-        
         {this.state.apiResponse}
       </div>
     );
