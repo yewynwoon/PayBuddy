a8768793c2beee0b896e617efbefb3334b236058
s3539822 <s3539822@student.rmit.edu.au>
2020-04-04 20:09:46 +1100

    CSS Updates, Page built
    Update css platform wide
    Build new page for 'pay bill' function

---
+++ paybuddy_v2/client/src/PayBill.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './course.css';
+import './paybill.css'
+
+class PayBill extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render () {
+        return (
+            <main id='cous'>
+                <div class="_3mYpM" id="header-id">
+                    <h2 class="_00004">Select deposit amount</h2>
+                </div>
+                <li class="_2j5BM">
+                    <div class="_1fm9z">
+                        <div class="_25xBd _2jWsC _2LHSO">
+                            <div class="_1GLWk" id="box-header-id"></div>
+                            <div class="rMY2T _3kqWw _2-iar" id="box-body-id">
+                                
+                                
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </main>
+        )
+    }
+}
+
+export default PayBill;
