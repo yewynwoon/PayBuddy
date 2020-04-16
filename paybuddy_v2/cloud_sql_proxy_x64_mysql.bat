b05540b8aae25cf7ca557422844ec1f2dbc5c546
s3539822 <s3539822@student.rmit.edu.au>
2020-04-16 13:38:22 +1000

    Updates

--- paybuddy_v2/cloud_sql_proxy_x64_mysql.bat
+++ paybuddy_v2/cloud_sql_proxy_x64_mysql.bat
@@ -1,2 +1,2 @@
-cloud_sql_proxy_x64.exe -instances=paybuddy-jeremy:australia-southeast1:paybuddy-mysql-db=tcp:1433
+cloud_sql_proxy_x64.exe -instances=paybuddy-2020:australia-southeast1:paybuddy-mysql-db=tcp:1433
 PAUSE
