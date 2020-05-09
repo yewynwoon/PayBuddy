da9bbb2411523b511019182cb0be4bd77761089d
Sam-A0 <53252880+Sam-A0@users.noreply.github.com>
2020-05-09 19:25:14 +1000

    establish connection to gcloud

---
+++ paybuddy_v2/admin/connection.php
@@ -0,0 +1,15 @@
+<?php
+    //connect to the SQL server in gcloud 
+    $db = mysqli_connect('34.87.251.98', 'root', 'hello', 'test' ,null,'paybuddy-2020:australia-southeast1:paybuddy-mysql-db');
+   
+    
+    $results = mysqli_query($db, "select * from users"); 
+    
+    //this is to test
+    while($row = mysqli_fetch_array($results))
+    {
+        print $row['fname'];
+
+    }
+
+?>
