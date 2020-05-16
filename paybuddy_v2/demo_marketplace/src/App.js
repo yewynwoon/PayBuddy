bc7e305dfaa55f2ab3ab96618f6dc21e2eb1aeb8
Ye Wyn <s3675485@student.rmit.edu.au>
2020-05-17 05:33:19 +1000

    wtf

--- paybuddy_v2/demo_marketplace/src/App.js
+++ paybuddy_v2/demo_marketplace/src/App.js
@@ -1,81 +1,23 @@
 import './App.css';
 import React, { useState, useRef, useEffect } from 'react';
 import chair from './chair.jpg';
+import Home from './Home.js'
 import gif from './giphy.gif';
 
-function onClick() {
-  return window.open("http://www.google.com");
-}
 
-function Product({ product }) {
-  const [paidFor, setPaidFor] = useState(false);
-  const [error, setError] = useState(null);
-  /* const payBuddyRef = useRef() */
-
-  /* useEffect(() => {
-    window.paypal
-      .Buttons({
-        createOrder: (data, actions) => {
-          return actions.order.create({
-            purchase_units: [
-              {
-                description: product.description,
-                amount: {
-                  currency_code: 'USD',
-                  value: product.price,
-                },
-              },
-            ],
-          });
-        },
-        onApprove: async (data, actions) => {
-          const order = await actions.order.capture();
-          setPaidFor(true);
-          console.log(order);
-        },
-        onError: err => {
-          setError(err);
-          console.error(err);
-        },
-      })
-      .render(paypalRef.current);
-  }, [product.description, product.price]); */
-
-  if (paidFor) {
-    return (
-      <div>
-        <h1>Congrats, you just bought {product.name}!</h1>
-        <img alt={product.description} src={gif} />
-      </div>
-    );
-  }
-
-  return (
-    <div>
-      {error && <div>Uh oh, an error occurred! {error.message}</div>}
-      <h1>
-        {product.description} for ${product.price}
-      </h1>
-      <img alt={product.description} src={product.image} width="200" />
-      
-      <button type="button" className="btn btn-primary" onClick={onClick} >
-        <span className="icon">Open</span>
-      </button>
-    </div>
-  );
-}
-
-function App() {
-  const product = {
+function App() 
+{
+  const product = 
+  {
     price: 777.77,
     name: 'comfy chair',
     description: 'fancy chair, like new',
     image: chair,
   };
 
-  return (
+  return(
     <div className="App">
-      <Product product={product} />
+      <Home/>
     </div>
   );
 }
