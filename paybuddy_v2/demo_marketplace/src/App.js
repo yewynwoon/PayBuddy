2e7051797d7c64f1b444cdfde8e23198e31fe495
Ye Wyn <s3675485@student.rmit.edu.au>
2020-05-18 23:09:12 +1000

    new

--- paybuddy_v2/demo_marketplace/src/App.js
+++ paybuddy_v2/demo_marketplace/src/App.js
@@ -1,25 +1,45 @@
 import './App.css';
 import React, { useState, useRef, useEffect } from 'react';
+import { BrowserRouter as Router, Route , useHistory} from 'react-router-dom';
+import {Security, SecureRoute, LoginCallback} from '@okta/okta-react';
+import config from './config';
 import chair from './chair.jpg';
 import Home from './Home.js'
 import gif from './giphy.gif';
 
 
-function App() 
-{
-  const product = 
-  {
-    price: 777.77,
-    name: 'comfy chair',
-    description: 'fancy chair, like new',
-    image: chair,
-  };
 
-  return(
-    <div className="App">
-      <Home/>
-    </div>
+const HasAccessToRouter = () => {
+  const history = useHistory();
+
+  const customAuthHandler = () => {
+    history.push('/login');
+  };
+  return (
+    <Security {...config.oidc}oAuthRequired={customAuthHandler}>
+      <div className="App">
+        <Route path="/" exact component={Home} />
+      </div>
+    </Security>
   );
+};
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+
+  render () {
+    return (
+    <div>
+      <Router>
+        <HasAccessToRouter/>
+      </Router>
+    </div>
+    );
+  }
 }
 
 export default App;
