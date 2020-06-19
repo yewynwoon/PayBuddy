c8a1eec839027da370b4a67ab0781697d3002600
rehanshams <58359995+rehanshams@users.noreply.github.com>
2020-06-19 23:59:40 +0500

    Update

--- paybuddy_v2/client/src/config.js
+++ paybuddy_v2/client/src/config.js
@@ -1,6 +1,6 @@
 const CLIENT_ID = process.env.CLIENT_ID || '0oaa2hazvvDXgKxtF4x6';
 const ISSUER = process.env.ISSUER || 'https://dev-203865.okta.com/oauth2/default';
-const OKTA_TESTING_DISABLEHTTPSCHECK = true;
+const OKTA_TESTING_DISABLEHTTPSCHECK = process.env.OKTA_TESTING_DISABLEHTTPSCHECK || false;
 
 export default {
   oidc: {
