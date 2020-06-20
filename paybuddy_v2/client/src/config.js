3219b3cee152297d0f410e27af88f7885e3e9983
Ye Wyn <s3675485@student.rmit.edu.au>
2020-06-20 14:12:12 +1000

    Updated config

--- paybuddy_v2/client/src/config.js
+++ paybuddy_v2/client/src/config.js
@@ -6,7 +6,7 @@ export default {
   oidc: {
     clientId: CLIENT_ID,
     issuer: ISSUER,
-    redirectUri: 'http://pay-buddy.com/implicit/callback',
+    redirectUri: 'https://pay-buddy.com/implicit/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: OKTA_TESTING_DISABLEHTTPSCHECK,
